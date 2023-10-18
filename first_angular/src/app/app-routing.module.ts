2b3dcb0a2485a0871275e511cc103c957db74dc6
hkimi amin <hkimiamin02@gmail.com>
2023-10-18 23:39:15 +0100

    add navbar

--- first_angular/src/app/app-routing.module.ts
+++ first_angular/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { AboutPageComponent } from './Pages/about-page/about-page.component';
+import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomePageComponent , title:'Home'},
+  {path:'about',component:AboutPageComponent , title:'About'},
+  {path:'contact',component:ContactPageComponent , title:'Contact'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- first_angular/src/app/app.module.ts
+++ first_angular/src/app/app.module.ts
@@ -9,20 +9,23 @@ import { MatIconModule } from '@angular/material/icon';
 import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { AboutPageComponent } from './Pages/about-page/about-page.component';
 import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
-
+import { NavbarComponent } from './Pages/layout/navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar'
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     AboutPageComponent,
-    ContactPageComponent
+    ContactPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
