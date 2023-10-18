b2b7e32cabb7371f41bb7655feb0ef481664da39
hkimi amin <hkimiamin02@gmail.com>
2023-10-18 22:52:16 +0100

    create components

--- first_angular/src/app/app.module.ts
+++ first_angular/src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { AboutPageComponent } from './Pages/about-page/about-page.component';
+import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
